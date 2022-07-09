85d64c817bd51f3c3c1f0394e8824f11e7bef7df
Ujwal Parashar <88845963+UjwalParashar9890@users.noreply.github.com>
2022-07-09 19:23:50 +0530

    Add files via upload

---
+++ continue.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module3
+# Purpose:
+#
+# Author:      ASUS
+#
+# Created:     09-07-2022
+# Copyright:   (c) ASUS 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+#break and continue statements
+students=["ram","shyam","radha","kishan","radhika"]
+
+for student in students:
+    if student=="radha":
+        break
+    print(student)
