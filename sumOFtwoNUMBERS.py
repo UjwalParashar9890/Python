ca37d3aca0f704f5649fc5aa1c5c859d603115be
Ujwal Parashar <88845963+UjwalParashar9890@users.noreply.github.com>
2022-07-09 19:03:54 +0530

    Add files via upload

---
+++ sumOFtwoNUMBERS.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      ASUS
+#
+# Created:     09-07-2022
+# Copyright:   (c) ASUS 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+first=input("Enter your first number: ")
+second=input("Enter your second number: ")
+sum=int(first)+int(second)
+print(sum)
