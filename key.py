38ee17f4388f488908b60acd7c38859fdf59cabf
Ujwal Parashar <88845963+UjwalParashar9890@users.noreply.github.com>
2022-07-09 19:40:51 +0530

    Add files via upload

---
+++ key.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module3
+# Purpose:
+#
+# Author:      ASUS
+#
+# Created:     09-07-2022
+# Copyright:   (c) ASUS 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+#key in python
+
+marks={"Physics":95,"Chemistry":97,"Mathematics":98}
+
+marks["Physics"]=99
+print(marks["Physics"])
+print(marks)
