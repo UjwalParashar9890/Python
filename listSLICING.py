8c720a44e6e1b74660e87e6e861071812797f84a
Ujwal Parashar <88845963+UjwalParashar9890@users.noreply.github.com>
2022-07-11 19:48:31 +0530

    Add files via upload

---
+++ listSLICING.py
@@ -0,0 +1,12 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      ASUS
+#
+# Created:     11-07-2022
+# Copyright:   (c) ASUS 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+friends=["ujwal","tushar","aniket","vaibhav","rahul","kalyan","shivam"]
+print(friends[0:3])
