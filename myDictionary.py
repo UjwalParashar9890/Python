04d0eaf674e4cf042fbf774bc5b93fbeb094a478
Ujwal Parashar <88845963+UjwalParashar9890@users.noreply.github.com>
2022-07-11 23:17:54 +0530

    Add files via upload

---
+++ myDictionary.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module4
+# Purpose:
+#
+# Author:      ASUS
+#
+# Created:     11-07-2022
+# Copyright:   (c) ASUS 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+#dictionary
+myDict={
+        "Fast":"In a quick manner",
+        "Ujwal":"simple boy",
+        "marks":[54,55,57,56],
+        "anotherDict":{"ujwal":"coder"}
+        }
+print(myDict['anotherDict']['ujwal'])
