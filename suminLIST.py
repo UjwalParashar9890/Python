19d75066c555ad689d7f76ae41786f8a2b2f8881
Ujwal Parashar <88845963+UjwalParashar9890@users.noreply.github.com>
2022-07-11 22:56:15 +0530

    Add files via upload

---
+++ suminLIST.py
@@ -0,0 +1,14 @@
+#-------------------------------------------------------------------------------
+# Name:        module3
+# Purpose:
+#
+# Author:      ASUS
+#
+# Created:     11-07-2022
+# Copyright:   (c) ASUS 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+a=[34,65,78,98]
+print(a[0]+a[1]+a[2]+a[3]) #not the best way
+print(sum(a)) #best way to sum in a list
