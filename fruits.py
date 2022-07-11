38ba51d25068147fc95b5477a3fa52d8bf990a66
Ujwal Parashar <88845963+UjwalParashar9890@users.noreply.github.com>
2022-07-11 22:51:07 +0530

    Add files via upload

---
+++ fruits.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module3
+# Purpose:
+#
+# Author:      ASUS
+#
+# Created:     11-07-2022
+# Copyright:   (c) ASUS 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+a=input("Enter fruit no.1!")
+b=input("Enter fruit no.2!")
+c=input("Enter fruit no.3!")
+d=input("Enter fruit no.4!")
+e=input("Enter fruit no.5!")
+f=input("Enter fruit no.6!")
+g=input("Enter fruit no.7!")
+MyFruitList=[a,b,c,d,e,f,g]
+MyFruitList.sort()
+print(MyFruitList)
---
+++ sortedMARKS.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module3
+# Purpose:
+#
+# Author:      ASUS
+#
+# Created:     11-07-2022
+# Copyright:   (c) ASUS 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+M1=input("enter your marks std1!")
+M2=input("enter your marks std2!")
+M3=input("enter your marks std3!")
+M4=input("enter your marks std4!")
+M5=input("enter your marks std5!")
+M6=input("enter your marks std6!")
+classMarks=[M1,M2,M3,M4,M5,M6]
+classMarks.sort()
+print(classMarks)
