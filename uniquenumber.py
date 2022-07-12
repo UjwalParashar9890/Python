71c4e83d5803d10a1b85693c3d5609310447d1e9
Ujwal Parashar <88845963+UjwalParashar9890@users.noreply.github.com>
2022-07-13 01:29:47 +0530

    Add files via upload

---
+++ uniquenumber.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      ASUS
+#
+# Created:     13-07-2022
+# Copyright:   (c) ASUS 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+num1=input("enter num1:")
+num2=input("enter num2:")
+num3=input("enter num3:")
+num4=input("enter num4:")
+num5=input("enter num5:")
+num6=input("enter num6:")
+num7=input("enter num7:")
+num8=input("enter num8:")
+a={num1,num2,num3,num4,num5,num6,num7,num8}
+print(a)
