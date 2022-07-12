a5dbebdfe3375f8835c122853ac83a92d0dfe89c
Ujwal Parashar <88845963+UjwalParashar9890@users.noreply.github.com>
2022-07-13 00:42:21 +0530

    Add files via upload

---
+++ wordmeaning.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      ASUS
+#
+# Created:     12-07-2022
+# Copyright:   (c) ASUS 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+myDict={
+"pankha":"fan",
+"dabba":"box",
+"vastu":"item"
+}
+print("options are",myDict.keys())
+a=input("Enter your hindi word")
+print("The meaning of your word is:",myDict[a])
