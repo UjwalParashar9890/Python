3b2be44188e1f8ebce3f43d0612077c9f1d4a40d
Ujwal Parashar <88845963+UjwalParashar9890@users.noreply.github.com>
2022-07-13 19:22:39 +0530

    Add files via upload

---
+++ username.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      ASUS
+#
+# Created:     13-07-2022
+# Copyright:   (c) ASUS 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+username=input("Enter a username")
+if(len(username)<10):
+    print("yes!")
+else:
+    print("no!")
