29b52eea3bf8631efb9722dae4b6bd9f12301314
Ujwal Parashar <88845963+UjwalParashar9890@users.noreply.github.com>
2022-07-13 16:24:44 +0530

    Add files via upload

---
+++ favLang.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      ASUS
+#
+# Created:     13-07-2022
+# Copyright:   (c) ASUS 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+favLang={}
+a=input("Enter your favLang ujwal:")
+b=input("Enter your favLang tushar:")
+c=input("Enter your favLang aniket:")
+d=input("Enter your favLang kanishka:")
+favLang['ujwal']=a
+favLang['tushar']=c
+favLang['tushar']=b
+favLang['kanishka']=d
+print(favLang)
