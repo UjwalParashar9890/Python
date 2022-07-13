089fe12bba8c74ba679ced550d667cb2e4921423
Ujwal Parashar <88845963+UjwalParashar9890@users.noreply.github.com>
2022-07-13 17:15:39 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      ASUS
+#
+# Created:     13-07-2022
+# Copyright:   (c) ASUS 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+a=int(input("enter your desired number"))
+if(a>10 and a<12):
+    print("hello!")
+elif(a<100 and a>90):
+    print("bye!")
+else:
+    print("let's see!")
