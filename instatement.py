b97e96fafb84be76f63fdee7a60ad931c9dcf9ba
Ujwal Parashar <88845963+UjwalParashar9890@users.noreply.github.com>
2022-07-13 19:36:30 +0530

    Add files via upload

---
+++ instatement.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        module2
+# Purpose:
+#
+# Author:      ASUS
+#
+# Created:     13-07-2022
+# Copyright:   (c) ASUS 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+a=input("enter a name")
+b=["ujwal","aniket","tushar"]
+if(a in b):
+    print("present!")
+else:
+    print("not present!")
