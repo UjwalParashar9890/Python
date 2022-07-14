55d5b2cf66628018d6fddc1643a197bb2b3882f9
Ujwal Parashar <88845963+UjwalParashar9890@users.noreply.github.com>
2022-07-14 13:05:35 +0530

    Add files via upload

---
+++ stringfinder.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      ASUS
+#
+# Created:     14-07-2022
+# Copyright:   (c) ASUS 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+a=["harry","Harry"]
+b=input("enter your post!")
+if(b in a):
+    print("Yes! it is talking about harry in this post")
+else:
+    print("N0! You are safe")
+
