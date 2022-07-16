201dc4845d9bba33810e21276e98d1972d94dac7
Ujwal Parashar <88845963+UjwalParashar9890@users.noreply.github.com>
2022-07-16 19:14:33 +0530

    Add files via upload

---
+++ startswith.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      ASUS
+#
+# Created:     16-07-2022
+# Copyright:   (c) ASUS 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+l1=["harry","rohan","sachin","sohail"]
+for name in l1:
+    if name.startswith("s"):
+        print("hello!" + name)
