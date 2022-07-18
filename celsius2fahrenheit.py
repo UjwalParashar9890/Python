0cfdc8fa39dc5eb7f6051dd1cb8f706df57a2064
Ujwal Parashar <88845963+UjwalParashar9890@users.noreply.github.com>
2022-07-18 17:56:44 +0530

    Add files via upload

---
+++ celsius2fahrenheit.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module6
+# Purpose:
+#
+# Author:      ASUS
+#
+# Created:     18-07-2022
+# Copyright:   (c) ASUS 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def farh(cel):
+    return (cel*(9/5))+32
+
+c=37
+f=farh(c)
+print("fahrenheit temperature is " + str(f))
