9daa02ea167a774b5a73cbc703d581a20a1c9141
Ujwal Parashar <88845963+UjwalParashar9890@users.noreply.github.com>
2022-07-27 19:05:58 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      ASUS
+#
+# Created:     25-07-2022
+# Copyright:   (c) ASUS 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+class Phone:
+    def make_call(self):
+        print("I am making a call")
+    def play_game(self):
+        print("I am playing a game")
+
+p1=Phone()
+p1.make_call()
